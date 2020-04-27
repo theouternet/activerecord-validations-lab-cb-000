6e3a32682557cfb24c246fe341ac500c44887bc6
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-27 19:03:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+
+validates :title, presence: true 
+    validates :name, uniqueness: true 
+    validates :phone_number, length: { :minimum 10 } 
+    validates :phone_number, length: { :maximum 10 } 
+
 end
