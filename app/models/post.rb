c497d717df2d5582907058529537cb967dbbbd00
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-27 19:49:32 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,6 @@ class Post < ActiveRecord::Base
     validates :summary, length: { maximum: 250 } 
     validates :category, inclusion: { in: %w(Fiction, Non-Fiction),
       message: "%{value} is invalid"}
+      
 
 end
