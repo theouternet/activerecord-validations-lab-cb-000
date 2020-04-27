c7a32b9b566473cbdc829fc84c5a936ff03bf50f
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-27 19:48:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ActiveRecord::Base
 
     validates :title, presence: true 
-    validates :name, uniqueness: true 
     validates :content, length: { minimum: 250 } 
     validates :summary, length: { maximum: 250 } 
     validates :category, inclusion: { in: %w(Fiction, Non-Fiction),
