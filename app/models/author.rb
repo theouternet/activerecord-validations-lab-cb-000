a0b73be98cddb9ef22d141339a28bc7e3d6c7678
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-27 18:44:50 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,7 @@ class Author < ActiveRecord::Base
   
   validates :name, presence: true 
     validates :name, uniqueness: true 
-    validates :phone_number, uniqueness: true 
+    validates :phone_number, length: { :minimum 10 } 
 
   
 end
